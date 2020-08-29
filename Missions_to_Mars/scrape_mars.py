cec2a33869f6ace47c3dc0f01f5db7788a69f894
Josh <joshua.dickenson91@gmail.com>
2020-08-29 16:52:40 +0800

    Updated HTML and Fixed Notebook

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,4 +1,4 @@
-# Dependencies
+#imports and dependacies
 from splinter import Browser
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -17,63 +17,64 @@ def scrape():
     browser = init_browser()
     mars_dict ={}
 
-    # Mars News URL of page to be scraped
+    #Url info
     news_url = 'https://mars.nasa.gov/news/'
     browser.visit(news_url)
     time.sleep(10)
     html = browser.html
     news_soup = BeautifulSoup(html, 'html.parser')
-    # Retrieve the latest news title and paragraph
+    #Retrieve the latest news title and paragraph
     news_title = news_soup.find_all('div', class_='content_title')[1].text
     news_p = news_soup.find_all('div', class_='article_teaser_body')[0].text
 
-    # Mars Image to be scraped
+    #Mars Image to be scraped url info
     jpl_nasa_url = 'https://www.jpl.nasa.gov'
     images_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
     browser.visit(images_url)
     html = browser.html
     images_soup = BeautifulSoup(html, 'html.parser')
-    # Retrieve featured image link
+    #Retrieve featured image link
     relative_image_path = images_soup.find_all('img')[3]["src"]
     featured_image_url = jpl_nasa_url + relative_image_path
 
-    # Mars facts to be scraped, converted into html table
+    #Mars facts to be scraped
     facts_url = 'https://space-facts.com/mars/'
     tables = pd.read_html(facts_url)
     mars_facts_df = tables[2]
     mars_facts_df.columns = ["Description", "Value"]
+    #create HTML Table
     mars_html_table = mars_facts_df.to_html()
     mars_html_table.replace('\n', '')
     
-    # Mars hemisphere name and image to be scraped
+    #Mars hemisphere name and image info
     usgs_url = 'https://astrogeology.usgs.gov'
     hemispheres_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(hemispheres_url)
     hemispheres_html = browser.html
     hemispheres_soup = BeautifulSoup(hemispheres_html, 'html.parser')
-    # Mars hemispheres products data
+    #Mars hemispheres data
     all_mars_hemispheres = hemispheres_soup.find('div', class_='collapsible results')
     mars_hemispheres = all_mars_hemispheres.find_all('div', class_='item')
     hemisphere_image_urls = []
-    # Iterate through each hemisphere data
-    for i in mars_hemispheres:
-        # Collect Title
-        hemisphere = i.find('div', class_="description")
+    
+    for hemi in mars_hemispheres:
+        #get title of pic
+        hemisphere = hemi.find('div', class_="description")
         title = hemisphere.h3.text        
-        # Collect image link by browsing to hemisphere page
+        #Get link for pic
         hemisphere_link = hemisphere.a["href"]    
         browser.visit(usgs_url + hemisphere_link)        
         image_html = browser.html
         image_soup = BeautifulSoup(image_html, 'html.parser')        
         image_link = image_soup.find('div', class_='downloads')
         image_url = image_link.find('li').a['href']
-        # Create Dictionary to store title and url info
+        #Create Mar Dict and store info
         image_dict = {}
         image_dict['title'] = title
         image_dict['img_url'] = image_url        
         hemisphere_image_urls.append(image_dict)
 
-    # Mars 
+    #Assemble the dictionary
     mars_dict = {
         "news_title": news_title,
         "news_p": news_p,
