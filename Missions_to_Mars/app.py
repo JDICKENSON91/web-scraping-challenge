f1fb7c2cc987c05186bc0e96de54f977503955a4
Josh <joshua.dickenson91@gmail.com>
2020-08-29 09:26:47 +0800

    Finished HTML

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,45 @@
+from splinter import Browser
+from bs4 import BeautifulSoup as bs
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+def init_browser():
+    # @NOTE: Replace the path with your actual path to the chromedriver
+    executable_path = {'executable_path':ChromeDriverManager().install()}
+    return Browser("chrome", **executable_path, headless=False)
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mission_to_mars")
+
+
+@app.route("/")
+def home():
+
+    # Find one record of data from the mongo database
+    mars_data = mongo.db.collection.find_one()
+
+    # Return template and data
+    return render_template("index.html", mars_data=mars_data)
+
+
+@app.route("/scrape")
+def scrape():
+
+    # Run the scrape function
+    mars_dict = scrape_mars.scrape_info()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.collection.update({}, mars_dict, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
